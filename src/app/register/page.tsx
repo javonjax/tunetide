d09b0eced97df4b40f6bde543daa81afc70d117d
javonjax <javonj461@hotmail.com>
2026-01-18 17:42:54 -0500

    Updated UI to accomodate OAuth login and registration.

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,11 +6,22 @@ import {
   emailValidation,
   registrationPasswordValidation,
 } from '@/lib/schemas';
+import { Github } from 'lucide-react';
 import Link from 'next/link';
 import React, { useContext, useEffect } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Image from 'next/image';
+import { ReadonlyURLSearchParams, useRouter, useSearchParams } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { APIError, errorToast, successToast } from '@/lib/utils';
+
+const GOOGLE_LOGO_URL: string = process.env.NEXT_PUBLIC_GOOGLE_LOGO as string;
+const GOOGLE_OAUTH_ENDPOINT: string = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_ENDPOINT as string;
+const GITHUB_OAUTH_ENDPOINT: string = process.env.NEXT_PUBLIC_GITHUB_OAUTH_ENDPOINT as string;
 
 const RegistrationPage = (): React.JSX.Element => {
+  const searchParams: ReadonlyURLSearchParams = useSearchParams();
+  const router: AppRouterInstance = useRouter();
   const authContext = useContext<AuthContextType | undefined>(AuthContext);
 
   /*
@@ -22,6 +33,36 @@ const RegistrationPage = (): React.JSX.Element => {
       authContext?.clearRegistrationError();
     };
   }, []);
+
+  /*
+      Handles OAuth registration/login success and errors.
+  */
+  useEffect(() => {
+    const handleOAuthLogin = async () => {
+      try {
+        const status: string | null = searchParams.get('status');
+        const provider: string | null = searchParams.get('provider');
+
+        if (status === 'success') {
+          await authContext?.updateSessionContext('login');
+          successToast('Login Successful', 'Enjoy your stay. Share your faves.');
+          router.replace('/');
+        } else if (status === 'fail') {
+          throw new APIError(`Error connecting to ${provider}. Please try again later.`, 500);
+        } else {
+          return;
+        }
+      } catch (error) {
+        if (error instanceof APIError) {
+          errorToast('Authentication error.', error.message);
+        } else {
+          errorToast('Authentication error.', 'There was an error authenticating this user.');
+        }
+        router.replace('/register');
+      }
+    };
+    handleOAuthLogin();
+  }, [searchParams, router]);
 
   const {
     register,
@@ -74,13 +115,36 @@ const RegistrationPage = (): React.JSX.Element => {
             Create account
           </button>
         </form>
-        <div className="flex flex-col text-[hsl(var(--text-color))]">
+        <div className="flex w-full flex-col items-center gap-2 text-[hsl(var(--text-color))]">
           <p>
             Already have an account?{' '}
             <Link href="/login" className="text-accent">
               Login here.
             </Link>
           </p>
+          <p className="text-[14px]">Or register in with</p>
+          <div className="flex w-full items-center justify-evenly">
+            <Link
+              href={`${GOOGLE_OAUTH_ENDPOINT}?source=register`}
+              className="flex w-[25%] flex-col items-center justify-center gap-2 rounded-2xl p-2"
+            >
+              <Image
+                width={24}
+                height={24}
+                alt="Google G Logo"
+                src={GOOGLE_LOGO_URL}
+                loading="lazy"
+              ></Image>
+              <p className="text-[14px]">Google</p>
+            </Link>
+            <Link
+              href={`${GITHUB_OAUTH_ENDPOINT}?source=register`}
+              className="flex w-[25%] flex-col items-center justify-center gap-2 rounded-2xl p-2"
+            >
+              <Github width={24} height={24} />
+              <p className="text-[14px]">GitHub</p>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
